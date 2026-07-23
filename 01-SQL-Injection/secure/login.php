d92384759bddaed4a26368845ea33a2137d063de
Temitayo Azeez <tayo147@yahoo.com>
2026-07-23 04:03:57 +0100

    Implement secure login using Prepared Statements

---
+++ 01-SQL-Injection/secure/login.php
@@ -0,0 +1,31 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "appsec_lab");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $stmt = $conn->prepare(
+        "SELECT * FROM users
+         WHERE username = ?
+         AND password = ?"
+    );
+
+    $stmt->bind_param("ss", $username, $password);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        echo "Login Successful";
+    } else {
+        echo "Invalid username or password";
+    }
+}
