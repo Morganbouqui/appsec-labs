575e9fb0210918ee9cb0fa6da340ced5ad4ee50d
Temitayo Azeez <tayo147@yahoo.com>
2026-07-23 03:13:39 +0100

    Add vulnerable SQL login example

--- 01-SQL-Injection/vulnerable/login.php
+++ 01-SQL-Injection/vulnerable/login.php
@@ -1,1 +1,26 @@
+<?php
 
+$conn = new mysqli("localhost", "root", "", "appsec_lab");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+}
+
+$sql = "SELECT * FROM users
+        WHERE username = '$username'
+        AND password = '$password'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "Login Successful";
+} else {
+    echo "Invalid username or password";
+}
